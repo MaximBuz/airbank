3084335301b28ee5ca9ebcdb8df7a1f6e1a70d60
Maxim Buz <mbuz.maxim@gmail.com>
2022-07-16 21:23:53 +0200

    feat: categories resolver

---
+++ backend/graphql/resolvers/categories.ts
@@ -0,0 +1,13 @@
+import { Category as CategoryType } from '@prisma/client';
+import { Context } from '../../context';
+
+export const Category = {
+  async transactions(_parent: CategoryType, _args: any, context: Context) {
+    const transactions = await context.prisma.transaction.findMany({
+      where: { accountId: _parent.id },
+      orderBy: { date: 'desc' },
+      include: { account: true },
+    });
+    return transactions;
+  },
+};
