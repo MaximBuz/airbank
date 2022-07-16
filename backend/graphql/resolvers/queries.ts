ea63b6048bda13dfcf0da943cd29c4122a9a4513
Maxim Buz <mbuz.maxim@gmail.com>
2022-07-16 21:48:18 +0200

    feat: query resolvers

---
+++ backend/graphql/resolvers/queries.ts
@@ -0,0 +1,67 @@
+import { Context } from '../../context';
+import { Prisma } from '@prisma/client';
+
+export async function getAccountById(_parent: undefined, _args: { id: string }, context: Context) {
+  const account = await context.prisma.account.findFirst({
+    where: { id: _args.id },
+    include: { transactions: true },
+  });
+  return account;
+}
+
+export async function getAllAccounts(_parent: undefined, _args: undefined, context: Context) {
+  const accounts = await context.prisma.account.findMany({
+    include: { transactions: true },
+  });
+  return accounts;
+}
+
+export async function getAllCategories(_parent: undefined, _args: undefined, context: Context) {
+  const categories = await context.prisma.category.findMany();
+  return categories;
+}
+
+export async function getTransactionById(_parent: undefined, _args: { id: string }, context: Context) {
+  const transaction = await context.prisma.transaction.findFirst({
+    where: { id: _args.id },
+    include: { category: true, account: true },
+  });
+  return transaction;
+}
+
+export async function getTransactions(
+  _parent: undefined,
+  _args: {
+    sortAsc?: boolean;
+    accountId?: string;
+    startMonth?: string;
+    endMonth?: string;
+    take?: number;
+    cursor?: string;
+  },
+  context: Context
+) {
+  // Grabbing arguments and providing defaults
+  const sortAsc = _args.sortAsc || false;
+  const accountId = _args.accountId || undefined;
+  const startMonth = _args.startMonth || '01-2022'; // those have to be transformed into dates!
+  const endMonth = _args.endMonth || '12-2022'; // those have to be transformed into dates!
+  const take = _args.take || 20;
+  const cursor = _args.cursor || null;
+
+  const searchParams: Prisma.TransactionFindManyArgs = {
+    take,
+    where: { accountId, date: { lte: new Date(endMonth), gte: new Date(startMonth) } },
+    orderBy: { date: sortAsc ? 'asc' : 'desc' },
+  };
+
+  if (cursor) {
+    searchParams.cursor = {
+      id: cursor,
+    };
+    searchParams.skip = 1;
+  }
+
+  const transactions = await context.prisma.transaction.findMany(searchParams);
+  return transactions;
+}
---
+++ backend/prisma/migrations/20220716192806_renamings/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Category" ALTER COLUMN "color" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Transaction" ALTER COLUMN "reference" DROP NOT NULL;
