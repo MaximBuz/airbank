389c02a8ef5c670e15f16e20cdf04ce2a5503e43
Maxim Buz <mbuz.maxim@gmail.com>
2022-07-16 21:47:35 +0200

    feat: sending prisma client as context to resolvers

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,12 @@
 import { ApolloServer } from 'apollo-server';
 import { typeDefs } from './graphql/typedefs';
 import { resolvers } from './graphql/resolvers';
+import { context } from './context';
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context,
   csrfPrevention: true,
   cache: 'bounded',
 });
