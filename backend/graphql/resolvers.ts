424eafd5f8e888b6ab48a43332b884aea74dd6f7
Maxim Buz <mbuz.maxim@gmail.com>
2022-07-16 21:50:40 +0200

    feat: hook up resolvers

--- backend/graphql/resolvers.ts
+++ backend/graphql/resolvers.ts
@@ -1,5 +1,23 @@
+import { Transaction } from './resolvers/transactions';
+import { Category } from './resolvers/categories';
+import { Account } from './resolvers/accounts';
+import {
+  getAccountById,
+  getAllAccounts,
+  getAllCategories,
+  getTransactionById,
+  getTransactions,
+} from './resolvers/queries';
+
 export const resolvers = {
   Query: {
-    //...
+    getAccountById,
+    getAllAccounts,
+    getAllCategories,
+    getTransactionById,
+    getTransactions,
   },
+  Account,
+  Category,
+  Transaction,
 };
