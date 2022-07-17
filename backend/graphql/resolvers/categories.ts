92a3645ebcdb72f369645f8851e3f32ea83bdc4f
Maxim Buz <mbuz.maxim@gmail.com>
2022-07-17 07:58:16 +0200

    fix: category resolver wrong query

--- backend/graphql/resolvers/categories.ts
+++ backend/graphql/resolvers/categories.ts
@@ -4,7 +4,7 @@ import { Context } from '../../context';
 export const Category = {
   async transactions(_parent: CategoryType, _args: undefined, context: Context) {
     const transactions = await context.prisma.transaction.findMany({
-      where: { accountId: _parent.id },
+      where: { categoryId: _parent.id },
       orderBy: { date: 'desc' },
       include: { account: true },
     });
